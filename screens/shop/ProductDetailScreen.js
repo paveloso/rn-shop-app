87d8a6c5c6549f05ddce1d619bfae3247dfd11ad
Pavel <pavel@cloudbs.ru>
2020-05-08 19:05:34 +0300

    implements cart screen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, Button, ScrollView } from 'react-native';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import * as cartActions from '../../store/actions/cart';
 import Colors from '../../constants/Colors';
 
 const ProductDetailScreen = props => {
@@ -11,11 +13,15 @@ const ProductDetailScreen = props => {
         state.products.availableProducts.find(prod => prod.id === productId)
     );
 
+    const dispatch = useDispatch();
+
     return (
         <ScrollView>
             <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
             <View style={styles.actions}>
-                <Button color={Colors.primary} title="Add To Cart" onPress={() => { }} />
+                <Button color={Colors.primary} title="Add To Cart" onPress={() => {
+                    dispatch(cartActions.addToCart(selectedProduct));
+                }} />
             </View>
             <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
             <Text style={styles.description}>{selectedProduct.description}</Text>
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import * as cartActions from '../../store/actions/cart';
 
 import ProductItem from '../../components/shop/ProductItem';
+import HeaderButton from '../../components/UI/HeaderButton';
 
 const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
-return (
-        <FlatList 
-            data={products} 
-            keyExtractor={item => item.id} 
-            renderItem={itemData => <ProductItem 
-                                        image={itemData.item.imageUrl} 
-                                        title={itemData.item.title} 
-                                        price={itemData.item.price}
-                                        onViewDetails={() => {
-                                            props.navigation.navigate('ProductDetail', { 
-                                                productId: itemData.item.id,
-                                                productTitle: itemData.item.title 
-                                            } );
-                                        }}
-                                        onAddToCart={() => {}}
-                                    />
-                                } 
+
+    const dispatch = useDispatch();
+
+    return (
+        <FlatList
+            data={products}
+            keyExtractor={item => item.id}
+            renderItem={itemData => <ProductItem
+                image={itemData.item.imageUrl}
+                title={itemData.item.title}
+                price={itemData.item.price}
+                onViewDetails={() => {
+                    props.navigation.navigate('ProductDetail', {
+                        productId: itemData.item.id,
+                        productTitle: itemData.item.title
+                    });
+                }}
+                onAddToCart={() => {
+                    dispatch(cartActions.addToCart(itemData.item));
+                }}
+            />
+            }
         />
     );
 };
 
-ProductsOverviewScreen.navigationOptions = {
-    headerTitle: 'All Products'
+ProductsOverviewScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'All Products',
+        headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title='Cart' iconName='md-cart' onPress={() => { 
+                navData.navigation.navigate('Cart');
+             }} />
+        </HeaderButtons>
+    }
 };
 
 const styles = StyleSheet.create({
